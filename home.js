28c26d2eaf357c08276f5a3820d93d9b6803e414
Christian <christianpaul.wiener@gmail.com>
2023-11-28 19:06:59 +0100

    add first idea of how to make a popout container with our registratio/login form if we click the button top right note this is not working properly rn but will be fixed by tomorrow

--- home.js
+++ home.js
@@ -1,12 +1,37 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+// JavaScript code for next and previous buttons (slider functionality)
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
 next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[0]);
+});
 
 prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1]) 
-})
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').prepend(items[items.length - 1]);
+});
+
+// JavaScript to toggle the modal and blur effect
+var loginButton = document.getElementById('loginButton');
+var modalContainer = document.getElementById('modalContainer');
+var blurBackground = document.querySelector('.background');
+var form = document.getElementById('container1')
+
+loginButton.addEventListener('click', function() {
+    modalContainer.classList.toggle('active');
+    blurBackground.classList.toggle('blur');
+    form.classList.toggle('container1');
+});
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container1');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
