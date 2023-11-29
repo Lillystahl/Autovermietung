d4867b61ec195c96e9077199e39d50c35c32aaeb
Christian <christianpaul.wiener@gmail.com>
2023-11-29 11:51:21 +0100

    add background blur draft update butto colors update search container opacity

--- home.js
+++ home.js
@@ -28,6 +28,8 @@ signInButton.addEventListener('click', () => {
 
 // Get the login button element
 var loginButton = document.getElementById('loginButton');
+const background = document.querySelector('.background');
+
 
 // Get the container element that you want to toggle visibility
 var container1 = document.querySelector('.container1');
@@ -37,9 +39,7 @@ var blurBackground = document.querySelector('.background');
 
 // Add a click event listener to the login button
 loginButton.addEventListener('click', function() {
+    background.classList.toggle('active');
     // Toggle the visibility of container1 by adding/removing the 'active' class
     container1.classList.toggle('active');
-
-    // Toggle the blur effect by adding/removing 'active' class to the blurred overlay
-    blurBackground.classList.toggle('active');
 });
